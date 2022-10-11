1981d87ac723087d4a7a93353f660bd52648c587
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-10-11 19:11:20 +0600

    Responsive added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import './Home.css'
 const Home = () => {
     const quizs = useLoaderData()
 
@@ -8,7 +9,7 @@ const Home = () => {
         <div>
             <h2 className='mt-5'>Take the Free Practice Test</h2>
             <p>By Click on '<strong>Take Quizes</strong>' Button you can answer some questions on mentioned topic,and it's totaly free!!  </p>
-            <div className='d-flex '>
+            <div className='quizs d-flex flex-lg-row flex-sm-column '>
                 {
                     quizs.data.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
                 }
